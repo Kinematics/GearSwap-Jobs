b6d2f7a8ad86317cef9d1a0c9599a6f67c49fadb
David Smith <dsmith@datasync.com>
2013-12-27 23:26:07 -0600

    Update gear

--- WHM.lua
+++ WHM.lua
@@ -4,7 +4,7 @@
 
 -- NOTE: This is a work in progress, experimenting.  Expect it to change frequently, and maybe include debug stuff.
 
--- Last Modified: 12/27/2013 10:22:28 PM
+-- Last Modified: 12/27/2013 11:17:58 PM
 
 -- IMPORTANT: Make sure to also get the Mote-Include.lua file to go with this.
 
@@ -231,7 +231,7 @@ function get_sets()
 		back="Umbra Cape",legs="Gendewitha Spats",feet="Gendewitha Galoshes"}
 
 	sets.defense.MDT = {main=staffs.PDT,sub="Achaq Grip",
-		head="Nahtirah Hat",neck="Twilight Torque",
+		head="Theophany Cap +1",neck="Twilight Torque",
 		body="Gendewitha Bliaut",hands="Yaoyotl Gloves",ring1=leftDarkRing,ring2="Shadow Ring",
 		back="Engulfer Cape",legs="Gendewitha Spats",feet="Gendewitha Galoshes"}
 
