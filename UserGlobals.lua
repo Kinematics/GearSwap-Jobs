d2d273b5ff5b352bac57fcbf49433e4c0f29415a
David Smith <dsmith@datasync.com>
2014-01-11 14:54:15 -0600

    Change local table name for clarity.

--- UserGlobals.lua
+++ UserGlobals.lua
@@ -4,41 +4,41 @@
 -- update.
 -- Creates the 'gear' table for reference in other files.
 --
--- Note: Function and table definitions should be added to gearInclude, but references to
+-- Note: Function and table definitions should be added to userInclude, but references to
 -- the contained tables via functions (such as for the obi function, below) use only
 -- the base 'gear' table.
 -------------------------------------------------------------------------------------------------------------------
 
-local gearInclude = {}
+local userInclude = {}
 
 -- Special gear info that may be useful across jobs.
-gearInclude.gear = {}
+userInclude.gear = {}
 
 
 -- Obis
-gearInclude.gear.Obi = {}
-gearInclude.gear.Obi.Light = "Korin Obi"
---gearInclude.gear.Obi.Dark = "Anrin Obi"
-gearInclude.gear.Obi.Fire = "Karin Obi"
-gearInclude.gear.Obi.Ice = "Hyorin Obi"
---gearInclude.gear.Obi.Wind = "Furin Obi"
---gearInclude.gear.Obi.Earth = "Dorin Obi"
-gearInclude.gear.Obi.Lightning = "Rairin Obi"
---gearInclude.gear.Obi.Water = "Suirin Obi"
+userInclude.gear.Obi = {}
+userInclude.gear.Obi.Light = "Korin Obi"
+--userInclude.gear.Obi.Dark = "Anrin Obi"
+userInclude.gear.Obi.Fire = "Karin Obi"
+userInclude.gear.Obi.Ice = "Hyorin Obi"
+--userInclude.gear.Obi.Wind = "Furin Obi"
+--userInclude.gear.Obi.Earth = "Dorin Obi"
+userInclude.gear.Obi.Lightning = "Rairin Obi"
+--userInclude.gear.Obi.Water = "Suirin Obi"
 
 
 -- Staffs
-gearInclude.gear.Staff = {}
-gearInclude.gear.Staff.HMP = 'Chatoyant Staff'
-gearInclude.gear.Staff.PDT = 'Earth Staff'
+userInclude.gear.Staff = {}
+userInclude.gear.Staff.HMP = 'Chatoyant Staff'
+userInclude.gear.Staff.PDT = 'Earth Staff'
 
 
 -- Add the obi for the given element if it matches either the current weather or day.
-function gearInclude.add_obi(spell_element)
+function userInclude.add_obi(spell_element)
 	if gear.Obi[spell_element] and (world.weather_element == spell_element or world.day_element == spell_element) then
 		equip({waist=gear.Obi[spell_element]})
 	end
 end
 
 
-return gearInclude
+return userInclude
