0757d9dfbf3d2aa40c8ec75aef55ce46b1927867
David Smith <dsmith@datasync.com>
2014-01-11 16:02:50 -0600

    Add utility functions from main include.

--- Mote-Utility.lua
+++ Mote-Utility.lua
@@ -42,9 +42,85 @@ function utility.binds_on_unload()
 	windower.send_command('bind !f12 input /ma Reset .Defense')
 end
 
+-------------------------------------------------------------------------------------------------------------------
+-- Function to easily change to a given macro set or book.  Book value is optional.
+-------------------------------------------------------------------------------------------------------------------
+
+function utility.set_macro_page(set,book)
+	if not tonumber(set) then error('Macro page: Set not a valid number ('..tostring(set)..')', 2) end
+
+	if book then
+		if not tonumber(book) then error('Macro page: Book not a valid number ('..tostring(book)..')', 2) end
+		windower.send_command('input /macro book '..tostring(book)..';wait .1;input /macro set '..tostring(set))
+	else
+		windower.send_command('input /macro set '..tostring(set))
+	end
+end
+
 
 -------------------------------------------------------------------------------------------------------------------
--- Utility functions for changing target types.
+-- Utility functions for vars or other data manipulation.
+-------------------------------------------------------------------------------------------------------------------
+
+-- Utility for splitting strings.  Obsoleted by revised library function.
+function utility.split(msg, delim)
+	local result = T{}
+
+	-- If no delimiter specified, just extract alphabetic words
+	if not delim or delim == '' then
+		for word in msg:gmatch("%a+") do
+			result[#result+1] = word
+		end
+	else
+		-- If the delimiter isn't in the message, just return the whole message
+		if string.find(msg, delim) == nil then
+			result[1] = msg
+		else
+			-- Otherwise use a capture pattern based on the delimiter to
+			-- extract text chunks.
+			local pat = "(.-)" .. delim .. "()"
+			local lastPos
+			for part, pos in msg:gmatch(pat) do
+				result[#result+1] = part
+				lastPos = pos
+			end
+			-- Handle the last field
+			if #msg > lastPos then
+				result[#result+1] = msg:sub(lastPos)
+			end
+		end
+	end
+	
+	return result
+end
+
+
+-- Invert a table such that the keys are values and the values are keys.
+-- Use this to look up the index value of a given entry.
+function utility.invert_table(t)
+	if t == nil then error('Attempting to invert table, received nil.', 2) end
+	
+	local i={}
+	for k,v in pairs(t) do 
+		i[v] = k
+	end
+	return i
+end
+
+-- Gets sub-tables based on baseSet from the string str that may be in dot form
+-- (eg: baseSet=sets, str='precast.FC', this returns sets.precast.FC).
+function utility.get_expanded_set(baseSet, str)
+	local cur = baseSet
+	for i in str:gmatch("[^.]+") do
+		cur = cur[i]
+	end
+	
+	return cur
+end
+
+
+-------------------------------------------------------------------------------------------------------------------
+-- Utility function for changing target types in an automatic manner.
 -------------------------------------------------------------------------------------------------------------------
 
 function utility.auto_change_target(spell, action, spellMap)
@@ -110,19 +186,8 @@ end
 
 
 -------------------------------------------------------------------------------------------------------------------
--- Other utility functions
+-- Utility function for automatically adjusting the waltz spell being used to match the HP needs.
 -------------------------------------------------------------------------------------------------------------------
-
-function utility.set_macro_page(set,book)
-	if not tonumber(set) then error('Macro page: Set not a valid number ('..tostring(set)..')', 2) end
-
-	if book then
-		if not tonumber(book) then error('Macro page: Book not a valid number ('..tostring(book)..')', 2) end
-		windower.send_command('input /macro book '..tostring(book)..';wait .1;input /macro set '..tostring(set))
-	else
-		windower.send_command('input /macro set '..tostring(set))
-	end
-end
 
 
 return utility
