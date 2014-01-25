116e51edb542078e56b6d07f71b357307215a489
David Smith <dsmith@datasync.com>
2014-01-25 17:11:18 -0600

    Re-enable obi/cape/ring automatic gear assignment since day and weather elements are working again.

--- Mote-Include.lua
+++ Mote-Include.lua
@@ -551,7 +551,7 @@ function get_default_precast_set(spell, action, spellMap, eventArgs)
 	local equipSet = {}
 
 	-- Update defintions for element-specific gear that can be used.
-	--set_spell_obi_cape_ring(spell)
+	set_spell_obi_cape_ring(spell)
 	set_weaponskill_gorget_belt(spell)
 	set_fastcast_staff(spell)
 	set_recast_staff(spell)
