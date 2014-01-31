f778efed98ebd1a27ee2447f0a7f18f13d568ac1
David Smith <dsmith@datasync.com>
2014-01-30 18:26:58 -0600

    Use gear.ElementalBelt.

--- DNC.lua
+++ DNC.lua
@@ -143,7 +143,7 @@ function init_gear_sets()
 	sets.precast.WS['Aeolian Edge'] = {ammo="Charis Feather",
 		head="Thaumas Hat",neck="Stoicheion Medal",ear1="Friomisi Earring",ear2="Hecate's Earring",
 		body="Manibozho Jerkin",hands="Buremte Gloves",ring1="Rajas Ring",ring2="Demon's Ring",
-		back="Toro Cape",waist="Thunder Belt",legs="Iuitl Tights",feet="Iuitl Gaiters"}
+		back="Toro Cape",waist=gear.ElementalBelt,legs="Iuitl Tights",feet="Iuitl Gaiters"}
 	
 	sets.precast.Skillchain = {hands="Charis Bangles +2"}
 	
