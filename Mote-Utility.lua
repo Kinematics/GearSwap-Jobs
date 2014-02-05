ff2d7f2e2fd852e87313ddccdaeac5ca6c5c1f20
David Smith <dsmith@datasync.com>
2014-02-05 14:48:24 -0600

    Check for recast timers on attempted cancel before allowing a cancel to happen.

--- Mote-Utility.lua
+++ Mote-Utility.lua
@@ -7,23 +7,44 @@
 -- Buff-cancelling utility functions.
 -------------------------------------------------------------------------------------------------------------------
 
+local cancel_spells_to_check = S{'Sneak', 'Spectral Jig', 'Trance', 'Monomi: Ichi', 'Utsusemi: Ichi'}
+local cancel_types_to_check = S{'Waltz', 'Samba'}
+
 -- Function to cancel buffs if they'd conflict with using the spell you're attempting.
 function cancel_conflicting_buffs(spell, action, spellMap, eventArgs)
-	if spell.english == 'Spectral Jig' and buffactive.sneak then
-		cast_delay(0.2)
-		send_command('cancel sneak')
-	elseif spell.english == 'Sneak' and spell.target.type == 'SELF' and buffactive.sneak then
-		send_command('cancel sneak')
-	elseif (spell.english == 'Trance' or spell.type=='Waltz') and buffactive['saber dance'] then
-		cast_delay(0.2)
-		send_command('cancel saber dance')
-	elseif spell.type=='Samba' and buffactive['fan dance'] then
-		cast_delay(0.2)
-		send_command('cancel fan dance')
-	elseif spell.english:startswith('Monomi') then
-		send_command('@wait 1.7;cancel sneak')
-	elseif spell.english == 'Utsusemi: Ichi' then
-		send_command('@wait 1.7;cancel copy image*')
+	if cancel_spells_to_check:contains(spell.english) or cancel_types_to_check:contains(spell.type) then
+		if spell.action_type == 'Ability' then
+			local abil_recasts = windower.ffxi.get_ability_recasts()
+			if abil_recasts[spell.index] > 0 then
+				add_to_chat(123,'Cancel abort: Ability waiting on recast.')
+				eventArgs.cancel = true
+				return
+			end
+		elseif spell.action_type == 'Magic' then
+			local spell_recasts = windower.ffxi.get_spell_recasts()
+			if spell_recasts[spell.index] > 0 then
+				add_to_chat(123,'Cancel abort: Spell waiting on recast.')
+				eventArgs.cancel = true
+				return
+			end
+		end
+		
+		if spell.english == 'Spectral Jig' and buffactive.sneak then
+			cast_delay(0.2)
+			send_command('cancel sneak')
+		elseif spell.english == 'Sneak' and spell.target.type == 'SELF' and buffactive.sneak then
+			send_command('cancel sneak')
+		elseif (spell.english == 'Trance' or spell.type=='Waltz') and buffactive['saber dance'] then
+			cast_delay(0.2)
+			send_command('cancel saber dance')
+		elseif spell.type=='Samba' and buffactive['fan dance'] then
+			cast_delay(0.2)
+			send_command('cancel fan dance')
+		elseif spell.english:startswith('Monomi') then
+			send_command('@wait 1.7;cancel sneak')
+		elseif spell.english == 'Utsusemi: Ichi' then
+			send_command('@wait 1.7;cancel copy image*')
+		end
 	end
 end
 
