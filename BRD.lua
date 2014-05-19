3ce90e591f9f6cc171d3b324c23a112ca15bee70
David Smith <dsmith@datasync.com>
2014-05-19 00:41:24 -0500

    Remove direct call to cancel_spell().

--- BRD.lua
+++ BRD.lua
@@ -314,7 +314,6 @@ function job_precast(spell, action, spellMap, eventArgs)
 	if spell.type == 'BardSong' then
 		-- Auto-Pianissimo
 		if spell.target.type == 'PLAYER' and not spell.target.charmed and not state.Buff['Pianissimo'] then
-			cancel_spell()
 			send_command('@input /ja "Pianissimo" <me>; wait 1.25; input /ma "'..spell.name..'" '..spell.target.name)
 			eventArgs.cancel = true
 			return
