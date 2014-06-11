1a2f8d6ad45cb1eab7771ff7c26b7a98a85e3710
David Smith <dsmith@datasync.com>
2014-06-10 23:22:41 -0500

    Clean up and fill in lots of missing stuff.

--- GEO.lua
+++ GEO.lua
@@ -18,7 +18,7 @@ end
 -- Setup vars that are user-dependent.  Can override this function in a sidecar file.
 function user_setup()
 	-- Options: Override default values
-	options.OffenseModes = {'Normal'}
+	options.OffenseModes = {'None', 'Normal'}
 	options.DefenseModes = {'Normal'}
 	options.WeaponskillModes = {'Normal'}
 	options.IdleModes = {'Normal', 'PDT'}
@@ -27,9 +27,11 @@ function user_setup()
 	options.MagicalDefenseModes = {'MDT'}
 
 	state.Defense.PhysicalMode = 'PDT'
-	
-	-- Default macro set/book
-	set_macro_page(1, 6)
+	state.OffenseMode = 'None'
+
+	gear.default.weaponskill_waist = "Windbuffet Belt"
+
+	select_default_macro_book()
 end
 
 -- Called when this job file is unloaded (eg: job change)
@@ -42,89 +44,117 @@ end
 
 -- Define sets and vars used by this job file.
 function init_gear_sets()
+
 	--------------------------------------
-	-- Start defining the sets
+	-- Precast sets
 	--------------------------------------
-	
-	-- Precast Sets
-	
+
 	-- Precast sets to enhance JAs
-	
+	sets.precast.JA.Bolster = {body="Bagua Tunic"}
+	sets.precast.JA['Life cycle'] = {body="Geomancy Tunic"}
+
 	-- Fast cast sets for spells
-	
-	sets.precast.FC = {
+
+	sets.precast.FC = {ammo="Impatiens",
 		head="Nahtirah Hat",ear2="Loquacious Earring",
 		body="Vanir Cotehardie",ring1="Prolix Ring",
 		back="Swith Cape +1",waist="Witful Belt",legs="Orvail Pants +1",feet="Chelona Boots +1"}
 
-	sets.precast.FC.Cure = {
-		head="Nahtirah Hat",ear2="Loquacious Earring",
-		body="Heka's Kalasiris",ring1="Prolix Ring",
-		back="Pahtli Cape",waist="Witful Belt",legs="Orvail Pants +1",feet="Chelona Boots +1"}
+	sets.precast.FC.Cure = set_combine(sets.precast.FC, {main="Tamaxchi",sub="Genbu's Shield",back="Pahtli Cape"})
 
-       
+	sets.precast.FC['Elemental Magic'] = set_combine(sets.precast.FC, {neck="Stoicheion Medal"})
+
+	
 	-- Weaponskill sets
 	-- Default set for any weaponskill that isn't any more specifically defined
+	sets.precast.WS = {
+		head="Nahtirah Hat",neck=gear.ElementalGorget,ear1="Bladeborn Earring",ear2="Steelflash Earring",
+		body="Vanir Cotehardie",hands="Yaoyotl Gloves",ring1="Rajas Ring",ring2="K'ayres Ring",
+		back="Refraction Cape",waist=gear.ElementalBelt,legs="Hagondes Pants",feet="Hagondes Sabots"}
 
 	-- Specific weaponskill sets.  Uses the base set if an appropriate WSMod version isn't found.
-	sets.precast.WS['Flash Nova'] = {
-		head="Hagondes Hat",neck="Stoicheion Medal",ear1="Friomisi Earring",ear2="Hecate's Earring",
-		body="Hagondes Coat",hands="Yaoyotl Gloves",ring2="Strendu Ring",
+	sets.precast.WS['Flash Nova'] = {ammo="Dosis Tathlum",
+		head="Hagondes Hat",neck="Eddy Necklace",ear1="Friomisi Earring",ear2="Hecate's Earring",
+		body="Hagondes Coat",hands="Yaoyotl Gloves",ring1="Acumen Ring",ring2="Strendu Ring",
 		back="Toro Cape",waist="Snow Belt",legs="Hagondes Pants",feet="Hagondes Sabots"}
 
 	sets.precast.WS['Starlight'] = {ear2="Moonshade Earring"}
 
 	sets.precast.WS['Moonlight'] = {ear2="Moonshade Earring"}
-	
-	
-	-- Midcast Sets
-	
+
+
+	--------------------------------------
+	-- Midcast sets
+	--------------------------------------
+
+	-- Base fast recast for spells
 	sets.midcast.FastRecast = {
 		head="Zelus Tiara",ear2="Loquacious Earring",
 		body="Hagondes Coat",hands="Bokwus Gloves",ring1="Prolix Ring",
 		back="Swith Cape +1",waist="Goading Belt",legs="Hagondes Pants",feet="Hagondes Sabots"}
-		
+
 	sets.midcast.Geomancy = {range="Matre Bell"}
 
-	sets.midcast.Cure = {
-		body="Heka's Kalasiris",hands="Bokwus Gloves",
+	sets.midcast.Cure = {main="Tamaxchi",sub="Genbu's Shield",
+		body="Heka's Kalasiris",hands="Bokwus Gloves",ring1="Haoma Ring",ring2="Sirona's Ring",
 		back="Swith Cape +1",legs="Nares Trews",feet="Hagondes Sabots"}
+	
+	sets.midcast.Curaga = sets.midcast.Cure
 
 	sets.midcast.Protectra = {ring1="Sheltered Ring"}
 
 	sets.midcast.Shellra = {ring1="Sheltered Ring"}
 
-	
-	-- Sets to return to when not performing an action.
-	
+
+	--------------------------------------
+	-- Idle/resting/defense/etc sets
+	--------------------------------------
+
 	-- Resting sets
 	sets.resting = {head="Nefer Khat +1",neck="Wiglen Gorget",
-		body="Heka's Kalasiris",ring1="Sheltered Ring",ring2="Paguroidea Ring",
-		legs="Nares Trews",feet="Chelona Boots +1"}
-	
+		body="Heka's Kalasiris",hands="Serpentes Cuffs",ring1="Sheltered Ring",ring2="Paguroidea Ring",
+		waist="Austerity Belt",legs="Nares Trews",feet="Chelona Boots +1"}
 
-	-- Idle sets (default idle set not needed since the other three are defined, but leaving for testing purposes)
+
+	-- Idle sets
+
+	sets.idle = {main="Bolelabunga",sub="Genbu's Shield",range="Matre Bell",
+		head="Nefer Khat +1",neck="Wiglen Gorget",ear1="Bloodgem Earring",ear2="Loquacious Earring",
+		body="Heka's Kalasiris",hands="Serpentes Cuffs",ring1="Sheltered Ring",ring2="Paguroidea Ring",
+		back="Umbra Cape",waist="Goading Belt",legs="Nares Trews",feet="Herald's Gaiters"}
+
+	sets.idle.PDT = {main="Bolelabunga",sub="Genbu's Shield",range="Matre Bell",
+		head="Nahtirah Hat",neck="Twilight Torque",ear1="Bloodgem Earring",ear2="Loquacious Earring",
+		body="Hagondes Coat",hands="Yaoyotl Gloves",ring1="Defending Ring",ring2="Paguroidea Ring",
+		back="Umbra Cape",waist="Goading Belt",legs="Nares Trews",feet="Herald's Gaiters"}
+
+	-- .Pet sets are for when Luopan is present.
+	sets.idle.Pet = {main="Bolelabunga",sub="Genbu's Shield",range="Matre Bell",
+		head="Nahtirah Hat",neck="Twilight Torque",ear1="Bloodgem Earring",ear2="Loquacious Earring",
+		body="Hagondes Coat",hands="Yaoyotl Gloves",ring1="Defending Ring",ring2="Paguroidea Ring",
+		back="Umbra Cape",waist="Goading Belt",legs="Nares Trews",feet="Herald's Gaiters"}
+
+	sets.idle.PDT.Pet = {main="Bolelabunga",sub="Genbu's Shield",range="Matre Bell",
+		head="Nahtirah Hat",neck="Twilight Torque",ear1="Bloodgem Earring",ear2="Loquacious Earring",
+		body="Hagondes Coat",hands="Yaoyotl Gloves",ring1="Defending Ring",ring2="Paguroidea Ring",
+		back="Umbra Cape",waist="Goading Belt",legs="Nares Trews",feet="Herald's Gaiters"}
+
+	-- .Indi sets are for when an Indi-spell is active.
+	sets.idle.Indi = set_combine(sets.idle, {legs="Bagua Pants"})
+	sets.idle.Pet.Indi = set_combine(sets.idle.Pet, {legs="Bagua Pants"})
+	sets.idle.PDT.Indi = set_combine(sets.idle.PDT, {legs="Bagua Pants"})
+	sets.idle.PDT.Pet.Indi = set_combine(sets.idle.Pet, {legs="Bagua Pants"})
 
 	sets.idle.Town = {main="Bolelabunga",sub="Genbu's Shield",range="Matre Bell",
 		head="Nefer Khat +1",neck="Wiglen Gorget",ear1="Bloodgem Earring",ear2="Loquacious Earring",
 		body="Heka's Kalasiris",hands="Serpentes Cuffs",ring1="Sheltered Ring",ring2="Paguroidea Ring",
-		back="Umbra Cape",waist="Goading Belt",legs="Nares Trews",feet="Herald's Gaiters"}
-	
-	sets.idle.Field = {main="Bolelabunga",sub="Genbu's Shield",range="Matre Bell",
-		head="Nefer Khat +1",neck="Wiglen Gorget",ear1="Bloodgem Earring",ear2="Loquacious Earring",
-		body="Heka's Kalasiris",hands="Serpentes Cuffs",ring1="Sheltered Ring",ring2="Paguroidea Ring",
-		back="Umbra Cape",waist="Goading Belt",legs="Nares Trews",feet="Herald's Gaiters"}
-
-	sets.idle.Field.PDT = {main="Bolelabunga",sub="Genbu's Shield",range="Matre Bell",
-		head="Nahtirah Hat",neck="Twilight Torque",ear1="Bloodgem Earring",ear2="Loquacious Earring",
-		body="Hagondes Coat",hands="Yaoyotl Gloves",ring1="Defending Ring",ring2="Paguroidea Ring",
 		back="Umbra Cape",waist="Goading Belt",legs="Nares Trews",feet="Herald's Gaiters"}
 
 	sets.idle.Weak = {main="Bolelabunga",sub="Genbu's Shield",range="Matre Bell",
 		head="Nefer Khat +1",neck="Wiglen Gorget",ear1="Bloodgem Earring",ear2="Loquacious Earring",
 		body="Heka's Kalasiris",hands="Serpentes Cuffs",ring1="Sheltered Ring",ring2="Paguroidea Ring",
 		back="Umbra Cape",waist="Goading Belt",legs="Nares Trews",feet="Herald's Gaiters"}
-	
+
 	-- Defense sets
 
 	sets.defense.PDT = {range="Matre Bell",
@@ -141,35 +171,31 @@ function init_gear_sets()
 
 	sets.latent_refresh = {waist="Fucho-no-obi"}
 
+
+	--------------------------------------
 	-- Engaged sets
+	--------------------------------------
 
 	-- Variations for TP weapon and (optional) offense/defense modes.  Code will fall back on previous
 	-- sets if more refined versions aren't defined.
 	-- If you create a set with both offense and defense modes, the offense mode should be first.
 	-- EG: sets.engaged.Dagger.Accuracy.Evasion
-	
+
 	-- Normal melee group
 	sets.engaged = {range="Matre Bell",
 		head="Zelus Tiara",neck="Peacock Charm",ear1="Bladeborn Earring",ear2="Steelflash Earring",
 		body="Vanir Cotehardie",hands="Bokwus Gloves",ring1="Rajas Ring",ring2="Paguroidea Ring",
 		back="Umbra Cape",waist="Goading Belt",legs="Hagondes Pants",feet="Hagondes Sabots"}
+
+	--------------------------------------
+	-- Custom buff sets
+	--------------------------------------
+
 end
 
 -------------------------------------------------------------------------------------------------------------------
 -- Job-specific hooks that are called to process player actions at specific points in time.
 -------------------------------------------------------------------------------------------------------------------
-
--- Set eventArgs.handled to true if we don't want any automatic gear equipping to be done.
--- Set eventArgs.useMidcastGear to true if we want midcast gear equipped on precast.
-function job_precast(spell, action, spellMap, eventArgs)
-
-end
-
--- Run after the general precast() is done.
-function job_post_precast(spell, action, spellMap, eventArgs)
-
-end
-
 
 -- Set eventArgs.handled to true if we don't want any automatic gear equipping to be done.
 function job_midcast(spell, action, spellMap, eventArgs)
@@ -179,22 +205,14 @@ function job_midcast(spell, action, spellMap, eventArgs)
 	end
 end
 
--- Run after the general midcast() is done.
-function job_post_midcast(spell, action, spellMap, eventArgs)
-
-end
-
--- Set eventArgs.handled to true if we don't want any automatic gear equipping to be done.
-function job_aftercast(spell, action, spellMap, eventArgs)
-
-end
-
-
 -------------------------------------------------------------------------------------------------------------------
 -- Customization hooks for idle and melee sets, after they've been automatically constructed.
 -------------------------------------------------------------------------------------------------------------------
 
 function customize_idle_set(idleSet)
+    if player.mpp < 51 then
+        idleSet = set_combine(idleSet, sets.latent_refresh)
+    end
 	return idleSet
 end
 
@@ -206,16 +224,17 @@ end
 -- General hooks for other events.
 -------------------------------------------------------------------------------------------------------------------
 
--- Called when the player's status changes.
-function job_status_change(newStatus,oldStatus)
-
-end
-
 -- Called when a player gains or loses a buff.
 -- buff == buff gained or lost
 -- gain == true if the buff was gained, false if it was lost.
 function job_buff_change(buff, gain)
-	--handle_equipping_gear(player.status)
+	if player.indi and not classes.CustomIdleGroups:contains('Indi')then
+		classes.CustomIdleGroups:append('Indi')
+		handle_equipping_gear(player.status)
+	elseif classes.CustomIdleGroups:contains('Indi') and not player.indi then
+		classes.CustomIdleGroups:clear()
+		handle_equipping_gear(player.status)
+	end
 end
 
 
@@ -223,15 +242,29 @@ end
 -- User code that supplements self-commands.
 -------------------------------------------------------------------------------------------------------------------
 
--- Called for custom player commands.
-function job_self_command(cmdParams, eventArgs)
-
-end
-
 -- Called by the 'update' self-command.
 function job_update(cmdParams, eventArgs)
-
+	classes.CustomIdleGroups:clear()
+	if player.indi then
+		classes.CustomIdleGroups:append('Indi')
+	end
 end
+
+
+function job_state_change(stateField, newValue, oldValue)
+	if stateField == 'OffenseMode' then
+		if newValue == 'Normal' then
+			disable('main','sub')
+		else
+			enable('main','sub')
+		end
+	elseif stateField == 'Reset' then
+		if state.OffenseMode == 'None' then
+			enable('main','sub')
+		end
+	end
+end
+
 
 -- Function to display the current relevant user state when doing an update.
 -- Return true if display was handled, and you don't want the default info shown.
@@ -242,4 +275,9 @@ end
 -------------------------------------------------------------------------------------------------------------------
 -- Utility functions specific to this job.
 -------------------------------------------------------------------------------------------------------------------
+
+-- Select default macro book on initial load or subjob change.
+function select_default_macro_book()
+	set_macro_page(1, 6)
+end
 
