d58a52f4154356e496531df15c0cae0e1402cfcc
David Smith <dsmith@datasync.com>
2014-07-22 02:52:04 -0500

    Add event handler to count hits during Impetus, and use that for determining whether to use the Tantra body on weaponskills.

--- MNK.lua
+++ MNK.lua
@@ -15,6 +15,12 @@ end
 function job_setup()
 	state.CombatForm = get_combat_form()
 	update_melee_groups()
+
+	state.Buff.Footwork = buffactive.Footwork or false
+	state.Buff.Impetus = buffactive.Impetus or false
+
+	info.impetus_hit_count = 0
+	windower.raw_register_event('action', on_action_for_impetus)
 end
 
 
@@ -33,6 +39,7 @@ function user_setup()
 	
 	state.FootworkWS = false
 	state.Buff.Footwork = buffactive.Footwork or false
+	state.Buff.Impetus = buffactive.Impetus or false
 
 	select_default_macro_book()
 end
@@ -291,9 +298,12 @@ end
 -- Run after the general precast() is done.
 function job_post_precast(spell, action, spellMap, eventArgs)
 	if spell.type == 'WeaponSkill' and not state.Defense.Active then
-		if buffactive.impetus and (spell.english == "Ascetic's Fury" or spell.english == "Victory Smite") then
-			equip(sets.impetus_body)
-		elseif buffactive.footwork and (spell.english == "Dragon's Kick" or spell.english == "Tornado Kick") then
+		if state.Buff.Impetus and (spell.english == "Ascetic's Fury" or spell.english == "Victory Smite") then
+			-- Need 6 hits at capped dDex, or 9 hits if dDex is uncapped, for Tantra to tie or win.
+			if (state.OffenseMode == 'Mod' and info.impetus_hit_count > 5) or (info.impetus_hit_count > 8) then
+				equip(sets.impetus_body)
+			end
+		elseif state.Buff.Footwork and (spell.english == "Dragon's Kick" or spell.english == "Tornado Kick") then
 			equip(sets.footwork_kick_feet)
 		end
 		
@@ -414,4 +424,70 @@ function select_default_macro_book()
 	end
 end
 
+
+-- Keep track of the current hit count while Impetus is up.
+function on_action_for_impetus(action)
+	if state.Buff.Impetus then
+		-- count melee hits by player
+		if action.actor_id == player.id then
+			if action.category == 1 then
+				for _,target in pairs(action.targets) do
+					for _,action in pairs(target.actions) do
+						-- Reactions (bitset):
+						-- 1 = evade
+						-- 2 = parry
+						-- 4 = block/guard
+						-- 8 = hit
+						-- 16 = JA/weaponskill?
+						-- If action.reaction has bits 1 or 2 set, it missed or was parried. Reset count.
+						if (action.reaction % 4) > 0 then
+							info.impetus_hit_count = 0
+						else
+							info.impetus_hit_count = info.impetus_hit_count + 1
+						end
+					end
+				end
+			elseif action.category == 3 then
+				-- Missed weaponskill hits will reset the counter.  Can we tell?
+				-- Reaction always seems to be 24 (what does this value mean? 8=hit, 16=?)
+				-- Can't tell if any hits were missed, so have to assume all hit.
+				-- Increment by the minimum number of weaponskill hits: 2.
+				for _,target in pairs(action.targets) do
+					for _,action in pairs(target.actions) do
+						-- This will only be if the entire weaponskill missed or was parried.
+						if (action.reaction % 4) > 0 then
+							info.impetus_hit_count = 0
+						else
+							info.impetus_hit_count = info.impetus_hit_count + 2
+						end
+					end
+				end
+			end
+		elseif action.actor_id ~= player.id and action.category == 1 then
+			-- If mob hits the player, check for counters.
+			for _,target in pairs(action.targets) do
+				if target.id == player.id then
+					for _,action in pairs(target.actions) do
+						-- Spike effect animation:
+						-- 63 = counter
+						-- ?? = missed counter
+						if action.has_spike_effect then
+							-- spike_effect_message of 592 == missed counter
+							if action.spike_effect_message == 592 then
+								info.impetus_hit_count = 0
+							elseif action.spike_effect_animation == 63 then
+								info.impetus_hit_count = info.impetus_hit_count + 1
+							end
+						end
+					end
+				end
+			end
+		end
+		
+		--add_to_chat(123,'Current Impetus hit count = ' .. tostring(info.impetus_hit_count))
+	else
+		info.impetus_hit_count = 0
+	end
+	
+end
 
